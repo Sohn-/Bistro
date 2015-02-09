ff98f58661db4e01d9dc75b430fa16318aea86c7
Sohn <ce401-27@ce401-27-HP>
2015-02-09 09:12:06 +0900

    couponDaoImpl 파일 만듦

---
+++ joojoo/src/main/java/joojoo/dao/CouponDaoImpl.java
@@ -0,0 +1,45 @@
+package joojoo.dao;
+
+import java.util.List;
+
+import joojoo.entity.Coupon;
+
+public class CouponDaoImpl implements CouponDao{
+
+	@Override
+	public int insertCoupon(Coupon coupon) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateDepartment(Coupon coupon) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteDepartment(String couponCode) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Coupon> getCouponsByUserId(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Coupon> getCouponsByOwnerId(String ownerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Coupon> getCouponsByStatus(String couponStatus) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
