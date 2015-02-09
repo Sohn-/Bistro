4641646830be28f07f1395209f0f7e7c1eda3e61
ansoog <ansoog@naver.com>
2015-02-09 15:38:14 +0900

    66666

--- joojoo/basic_info.sql
+++ joojoo/basic_info.sql
@@ -2,15 +2,40 @@
 
 insert into persons (persons_code, persons_level)
 values(0,'4명 이하');
-
 insert into persons (persons_code, persons_level)
 values(1,'5~10명');
-
 insert into persons (persons_code, persons_level)
 values(2,'10명 이상');
 
 insert into service_type (service_type_code, service_type_name)
-values(0,'할인');
-
+values(0,'금액 할인');
 insert into service_type (service_type_code, service_type_name)
 values(1,'서비스 메뉴 제공');
+
+insert into region (region_code, region_name)
+values(0,'강남');
+insert into region (region_code, region_name)
+values(1,'건대');
+insert into region (region_code, region_name)
+values(2,'신림');
+insert into region (region_code, region_name)
+values(3,'신촌');
+insert into region (region_code, region_name)
+values(4,'이태원');
+insert into region (region_code, region_name)
+values(5,'종로');
+
+
+insert into store_type (type_code, type_name)
+values(0,'횟집');
+insert into store_type (type_code, type_name)
+values(1,'고깃집');
+insert into store_type (type_code, type_name)
+values(2,'포차');
+insert into store_type (type_code, type_name)
+values(3,'호프');
+insert into store_type (type_code, type_name)
+values(4,'이자카야');
+insert into store_type (type_code, type_name)
+values(5,'종로');
+
