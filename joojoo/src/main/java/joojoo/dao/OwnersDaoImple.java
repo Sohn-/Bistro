04f8369a03cd29b6f2bac9f37d22ec1998bf0dea
ce401-30 <ce401-30@ce401-30-HP>
2015-02-09 10:15:45 +0900

    커밋일단 해봄 재용

---
+++ joojoo/src/main/java/joojoo/dao/OwnersDaoImple.java
@@ -0,0 +1,60 @@
+package joojoo.dao;
+
+import joojoo.entity.Owners;
+
+
+public class OwnersDaoImple implements OwnersDao {
+
+	@Override
+	public int insertOwner(Owners owner) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateOwner(Owners owner) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteOwner(String ownerId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Owners getOwnersByOwnerId(String ownerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Owners getOwnersByOwnerMail(String ownerMail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Owners getOwnersByLicenseNumber(int licenseNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Owners getOwnersForIdFind(String ownerId, String ownerName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Owners getOwnersForPassWordFind(String ownerId, String ownerName,
+			String ownerMail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+	
+
+}
