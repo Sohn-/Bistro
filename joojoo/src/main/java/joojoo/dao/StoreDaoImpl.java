039592ee09b2d3ee78a28ae914e8643e19043813
Sohn <ce401-27@ce401-27-HP>
2015-02-09 10:36:25 +0900

    다시2

---
+++ joojoo/src/main/java/joojoo/dao/StoreDaoImpl.java
@@ -0,0 +1,52 @@
+package joojoo.dao;
+
+import java.util.List;
+
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import joojoo.entity.Stores;
+
+
+public class StoreDaoImpl implements StoreDao{
+	
+	@Autowired
+	SqlSessionTemplate sqlSession;
+	
+	private final String nameSpace = "joojoo.mappers.storeMapper.";
+
+	@Override
+	public int insertStore(Stores store) {
+		String stmt = nameSpace + "insertStore";
+		return sqlSession.insert(stmt, store);
+	}
+
+	@Override
+	public int updateStore(Stores store) {
+		String stmt = nameSpace + "updateStore";
+		return sqlSession.update(stmt, store);
+	}
+
+	@Override
+	public int deleteStore(int storeCode) {
+		String stmt = nameSpace + "deleteStore";
+		return sqlSession.delete(stmt, storeCode);
+	}
+
+	@Override
+	public List<Stores> getStoresByOwnerId(String ownerId) {
+		String stmt = nameSpace + "getStoresByOwnerId";
+		List<Stores> result = sqlSession.selectList(stmt,ownerId);
+		return result;
+	}
+
+	@Override
+	public Stores getStoreByStoreCode(int storeCode) {
+		String stmt = nameSpace + "getStoreByStoreCode";
+		Stores result = sqlSession.selectOne(stmt,storeCode);
+		return result;
+	}
+	
+
+}
---
+++ joojoo/src/main/java/joojoo/dao/UsersDaoImple.java
@@ -0,0 +1,58 @@
+package joojoo.dao;
+
+import java.util.List;
+
+import joojoo.entity.Stores;
+import joojoo.entity.Users;
+
+
+
+public class UsersDaoImple implements UsersDao{
+
+	@Override
+	public int insertUser(Users user) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateUser(Users user) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteUser(String userId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Users getUsersByUserId(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Users getUsersByUserMail(String userMail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Users getUsersForIdFind(String userId, String userName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Users getUsersForPassWordFind(String userId, String userName,
+			String userMail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+	
+
+}
