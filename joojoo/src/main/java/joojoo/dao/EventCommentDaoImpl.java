6a420ab1ca510c919477cd28f7859e4a70a6f299
Sohn <ce401-27@ce401-27-HP>
2015-02-09 13:46:40 +0900

    ewfwef

--- joojoo/src/main/java/joojoo/dao/EventCommentDaoImpl.java
+++ joojoo/src/main/java/joojoo/dao/EventCommentDaoImpl.java
@@ -3,6 +3,7 @@ package joojoo.dao;
 import java.util.List;
 
 import joojoo.entity.Coupon;
+import joojoo.entity.EventComment;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
@@ -12,15 +13,60 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class EventCommentDaoImpl implements CouponDao{
+public class EventCommentDaoImpl implements EventCommentDao{
 	static final Logger logger = LoggerFactory.getLogger(EventCommentDaoImpl.class);
 	@Autowired
 	@Qualifier("sqlSessionTemplate")
 	private SqlSessionTemplate sqlSession;
-	
-	private final String nameSpace = "joojoo.mapper.CouponMapper.";
+	private final String nameSpace = "joojoo.mapper.EventCommentMapper.";
 
+	
 	@Override
+	public int insertEventComment(EventComment eventComment) {
+		String statement = nameSpace + "insertEventComment";
+		int result = sqlSession.insert(statement, eventComment);
+		
+		logger.trace("insertEventComment종료...");
+		
+		return result;
+	}
+	@Override
+	public int updateEventComment(EventComment eventComment) {
+		String statement = nameSpace + "updateEventComment";
+		int result = sqlSession.update(statement, eventComment);
+		
+		logger.trace("updateEventComment종료...");
+		return result;
+	}
+	@Override
+	public int deleteEventComment(int commentCode) {
+		String statement = nameSpace + "deleteEventComment";
+		int result = sqlSession.delete(statement, commentCode);
+		
+		logger.trace("deleteEventComment종료...");
+		return result;
+	}
+	
+	//int regionCode, int typeCode, int personsCode, int serviceTypeCode
+	@Override
+	public List<EventComment> getEventCommentsByCategory(EventComment eventComment) {
+		String statement = nameSpace + "getEventCommentsByCategory";
+		List<EventComment> result = sqlSession.selectList(statement, eventComment);
+		
+		return result;
+	}
+	@Override
+	public List<EventComment> getEventCommentsBySearchKeyword(String keyword) {
+		String statement = nameSpace + "getEventCommentsBySearchKeyword";
+		List<EventComment> result = sqlSession.selectList(statement, keyword);
+		
+		return result;
+	}
+	
+	
+	
+
+	/*@Override
 	public int insertCoupon(Coupon coupon) {
 		String statement = nameSpace + "insertCoupon";
 		int result = sqlSession.insert(statement, coupon);
@@ -70,6 +116,6 @@ public class EventCommentDaoImpl implements CouponDao{
 		List<Coupon> result = sqlSession.selectList(statement, couponStatusCode);
 		
 		return result;
-	}
+	}*/
 
 }
