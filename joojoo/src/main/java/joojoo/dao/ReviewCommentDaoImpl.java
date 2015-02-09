ef3bca2af47734be4e2f15f9ba1990ee7b634587
Sohn <ce401-27@ce401-27-HP>
2015-02-09 20:06:18 +0900

    ff

--- joojoo/src/main/java/joojoo/dao/ReviewCommentDaoImpl.java
+++ joojoo/src/main/java/joojoo/dao/ReviewCommentDaoImpl.java
@@ -1,28 +1,33 @@
-/*package joojoo.dao;
+package joojoo.dao;
 
 import java.util.List;
 
+import joojoo.entity.Category;
+import joojoo.entity.Coupon;
+import joojoo.entity.EventComment;
 import joojoo.entity.ReviewComment;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
+public class ReviewCommentDaoImpl implements ReviewCommentDao{
 	static final Logger logger = LoggerFactory.getLogger(ReviewCommentDaoImpl.class);
-	
 	@Autowired
-	@Qualifier("sqlSessionTemplate")
-	private SqlSessionTemplate sqlSession;
-
+	//@Qualifier("sqlSessionTemplate")
+	SqlSessionTemplate sqlSession;
 	private final String nameSpace = "joojoo.mapper.ReviewCommentMapper.";
-
+	
 	@Override
 	public int insertReviewComment(ReviewComment reviewComment) {
 		String statement = nameSpace + "insertReviewComment";
 		int result = sqlSession.insert(statement, reviewComment);
+		
+		logger.trace("insertReviewComment종료...");
+		
 		return result;
 	}
 
@@ -30,6 +35,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 	public int updateReviewComment(ReviewComment reviewComment) {
 		String statement = nameSpace + "updateReviewComment";
 		int result = sqlSession.update(statement, reviewComment);
+		
+		logger.trace("updateReviewComment종료...");
 		return result;
 	}
 
@@ -37,6 +44,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 	public int deleteReviewComment(int commentCode) {
 		String statement = nameSpace + "deleteReviewComment";
 		int result = sqlSession.delete(statement, commentCode);
+		
+		logger.trace("deleteReviewComment종료...");
 		return result;
 	}
 
@@ -51,16 +60,61 @@ import org.springframework.beans.factory.annotation.Qualifier;
 	@Override
 	public List<ReviewComment> getReviewCommentsByStoreCode(String storeCode) {
 		String statement = nameSpace + "getReviewCommentsByStoreCode";
-		List<ReviewComment> result = sqlSession.selectList(statement, storeCode);		
+		List<ReviewComment> result = sqlSession.selectList(statement, storeCode);
+		
 		return result;
 	}
 
 	@Override
 	public List<ReviewComment> getReviewCommentsBySearchKeyword(String keyword) {
 		String statement = nameSpace + "getReviewCommentsBySearchKeyword";
-		List<ReviewComment> result = sqlSession.selectList(statement, keyword);		
+		List<ReviewComment> result = sqlSession.selectList(statement, keyword);
+		
 		return result;
 	}
+	
 
-}
+	
+	/*@Override
+	public int insertEventComment(EventComment eventComment) {
+		String statement = nameSpace + "insertEventComment";
+		int result = sqlSession.insert(statement, eventComment);
+		
+		logger.trace("insertEventComment종료...");
+		
+		return result;
+	}
+	@Override
+	public int updateEventComment(EventComment eventComment) {
+		String statement = nameSpace + "updateEventComment";
+		int result = sqlSession.update(statement, eventComment);
+		
+		logger.trace("updateEventComment종료...");
+		return result;
+	}
+	@Override
+	public int deleteEventComment(int commentCode) {
+		String statement = nameSpace + "deleteEventComment";
+		int result = sqlSession.delete(statement, commentCode);
+		
+		logger.trace("deleteEventComment종료...");
+		return result;
+	}
+	
+	//int regionCode, int typeCode, int personsCode, int serviceTypeCode
+	@Override
+	public List<EventComment> getEventCommentsByCategory(Category category) {
+		String statement = nameSpace + "getEventCommentsByCategory";
+		List<EventComment> result = sqlSession.selectList(statement, category);
+		
+		return result;
+	}
+	@Override
+	public List<EventComment> getEventCommentsBySearchKeyword(Category category) {
+		String statement = nameSpace + "getEventCommentsBySearchKeyword";
+		List<EventComment> result = sqlSession.selectList(statement, category);
+		
+		return result;
+	}
 */
+}
---
+++ joojoo/src/main/java/joojoo/test/ReviewCommentDaoImplTest.java
@@ -0,0 +1,85 @@
+package joojoo.test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import joojoo.dao.EventCommentDao;
+import joojoo.entity.Category;
+import joojoo.entity.EventComment;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class ReviewCommentDaoImplTest {
+static final Logger LOG = LoggerFactory.getLogger(ReviewCommentDaoImplTest.class);
+	
+	@Autowired
+	@Qualifier("EventComment")
+	EventCommentDao dao;
+	
+	public ReviewCommentDaoImplTest(){
+		ApplicationContext ctx = new GenericXmlApplicationContext("spring/application-config.xml");
+		dao = ctx.getBean(EventCommentDao.class);
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		ReviewCommentDaoImplTest test = new ReviewCommentDaoImplTest();
+		try {
+			test.eventCommentTest();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void eventCommentTest() throws ParseException{
+		/*String title = "자바로삽입";
+		String content = "자바로삽입";*/
+		
+/*		insert into event_comment(title, content, start_date, end_date, store_code, persons_code, service_type_code)
+		values('제목','내용',to_date(sysdate, 'yyyy-mm-dd'), to_date(sysdate, 'yyyy-mm-dd'), 1, 0, 0);*/
+		
+/*		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		String start = "2015-02-09";
+		Date startDate = dateFormat.parse(start);
+		String end = "2015-02-09";
+		Date endDate = (Date) dateFormat.parse(end);
+		int storeCode = 1;
+		int personsCode = 0;
+		int serviceTypeCode = 0;
+		
+
+
+		EventComment eventComment = new EventComment(title, content, startDate, endDate, storeCode, personsCode, serviceTypeCode);*/
+
+
+
+		//dao.insertEventComment(eventComment);
+		
+		//eventComment.setTitle("변경됨");
+		//eventComment.setCommentCode(5);
+		//dao.updateEventComment(eventComment);
+		
+		//dao.deleteEventComment(5);
+		
+		int regionCode = 0;
+		int typeCode = 0;
+		int personsCode = 0;
+		int serviceTypeCode = 0;
+		String keyword="자바로삽입";
+		Category category = new Category(regionCode, typeCode, personsCode, serviceTypeCode, keyword);
+		//dao.getEventCommentsByCategory(category);
+		
+		
+		dao.getEventCommentsBySearchKeyword(category);
+
+	}
+}
