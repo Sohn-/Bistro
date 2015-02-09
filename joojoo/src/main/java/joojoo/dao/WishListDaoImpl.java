259f23b3b81c65f9c93e1e596aa3bfc1078bb568
ansoog <ansoog@naver.com>
2015-02-09 10:24:51 +0900

    suji

---
+++ joojoo/src/main/java/joojoo/dao/WishListDaoImpl.java
@@ -0,0 +1,25 @@
+package joojoo.dao;
+
+import joojoo.entity.Users;
+import joojoo.entity.WishList;
+
+
+
+public class WishListDaoImpl implements WishListDao{
+
+	@Override
+	public int insertWishList(WishList wishList) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteWishList(int wishListCode) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+	
+
+}
