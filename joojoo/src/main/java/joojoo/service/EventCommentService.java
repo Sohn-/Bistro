d572214e5b804b6bf3d4c6a05f72efa816427253
Sohn <ce401-27@ce401-27-HP>
2015-02-10 16:39:22 +0900

    이벤트서비스 완료

--- joojoo/src/main/java/joojoo/service/EventCommentService.java
+++ joojoo/src/main/java/joojoo/service/EventCommentService.java
@@ -5,16 +5,17 @@ import java.util.List;
 import joojoo.entity.Category;
 import joojoo.entity.Coupon;
 import joojoo.entity.EventComment;
+import joojoo.entity.Stores;
 
 public interface EventCommentService {
 	
 	List<EventComment> SeachByKeyword(Category category); //키워드로 이벤트 검색
 	List<EventComment> SeachByCategory(Category category); //카테고리 이벤트 검색
 	List<EventComment> SeachMyEvent(String ownerId); //업주회원 내 이벤트 조회
-	int registEvent(String ownerId); //업주회원 마이페이지 이벤트 등록
-	int modifyEvent(int comment_code); //업주회원 마이페이지 이벤트 수정
-	int requestDeleteEvent(int comment_code); //업주회원 마이페이지 삭제 요청
-	EventComment SearchOneEvent(int comment_code); //이벤트 하나 상세 조회	
+	int registEvent(EventComment eventComment, Stores store); //업주회원 마이페이지 이벤트 등록
+	int modifyEvent(EventComment eventComment); //업주회원 마이페이지 이벤트 수정
+	int requestDeleteEvent(EventComment eventComment); //업주회원 마이페이지 삭제 요청
+	EventComment SearchOneEvent(int commentCode); //이벤트 하나 상세 조회	
 	
 
 }
