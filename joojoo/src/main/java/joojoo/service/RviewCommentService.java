7dc9771a068da9baea5cb035eb45ff9ed2e1d93b
송희츄 <hc426797@nate.com>
2015-02-10 18:36:39 +0900

    g

--- joojoo/src/main/java/joojoo/service/RviewCommentService.java
+++ joojoo/src/main/java/joojoo/service/RviewCommentService.java
@@ -3,20 +3,30 @@ package joojoo.service;
 import java.util.List;
 
 import joojoo.entity.Category;
-import joojoo.entity.Coupon;
 import joojoo.entity.EventComment;
+import joojoo.entity.RviewComment;
 import joojoo.entity.Stores;
 
 public interface RviewCommentService {
 	
-	List<EventComment> SeachByKeyword(Category category); //키워드로 이벤트 검색
-	List<RviewComment> Search
-	List<EventComment> SeachByCategory(Category category); //카테고리 이벤트 검색
-	List<EventComment> SeachMyEvent(String ownerId); //업주회원 내 이벤트 조회
-	int registEvent(EventComment eventComment, Stores store); //업주회원 마이페이지 이벤트 등록
-	int modifyEvent(EventComment eventComment); //업주회원 마이페이지 이벤트 수정
+	List<RviewComment> SearchAll();
+	List<RviewComment> SearchByUserId(String userId);
+	List<RviewComment> SearchByMyId(String userId);
+	List<RviewComment> SearchByTitle(String title);
+	List<RviewComment> SearchByContetnt(String content);
+	List<RviewComment> SearchByStoerName(String storeName);
+	List<RviewComment> SearchByKeyword(Category category); //키워드로 이벤트 검색
+		
+	//List<RviewComment> SeachByCategory(Category category); //카테고리 이벤트 검색
+	//List<RviewComment> SeachMyEvent(String ownerId); //업주회원 내 이벤트 조회
+	
+	int registEvent(RviewComment eventComment, Stores store); //업주회원 마이페이지 이벤트 등록
+	int modifyEvent(RviewComment eventComment); //업주회원 마이페이지 이벤트 수정
+	int deleteEvent(EventComment eventComment); //업주회원 마이페이지 삭제 요청
 	int requestDeleteEvent(EventComment eventComment); //업주회원 마이페이지 삭제 요청
-	EventComment SearchOneEvent(int commentCode); //이벤트 하나 상세 조회	
+	
+	EventComment SearchOneRviewComment(int commentCode); //이벤트 하나 상세 조회	
+	//이미지 등록 이미지 업로드 유틸
 	
 
 }
--- joojoo/src/main/java/joojoo/service/RviewCommentServiceImpl.java
+++ joojoo/src/main/java/joojoo/service/RviewCommentServiceImpl.java
@@ -3,9 +3,9 @@ package joojoo.service;
 import java.util.List;
 
 import joojoo.dao.RviewCommentDao;
-import joojoo.dao.StoreDao;
 import joojoo.entity.Category;
 import joojoo.entity.EventComment;
+import joojoo.entity.RviewComment;
 import joojoo.entity.Stores;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +19,77 @@ public class RviewCommentServiceImpl implements RviewCommentService {
 
 	@Autowired
 	RviewCommentDao dao;
+
+	@Override
+	public List<RviewComment> SearchAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<RviewComment> SearchByUserId(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<RviewComment> SearchByMyId(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<RviewComment> SearchByTitle(String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<RviewComment> SearchByContetnt(String content) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<RviewComment> SearchByStoerName(String storeName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<RviewComment> SearchByKeyword(Category category) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int registEvent(RviewComment eventComment, Stores store) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int modifyEvent(RviewComment eventComment) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteEvent(EventComment eventComment) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int requestDeleteEvent(EventComment eventComment) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public EventComment SearchOneRviewComment(int commentCode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
