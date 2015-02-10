155b72b7802d8c3ac1e3b57e837c291924fcd38c
Sohn <ce401-27@ce401-27-HP>
2015-02-10 09:27:22 +0900

    final erm 수정

--- joojoo/basic_info.sql
+++ joojoo/basic_info.sql
@@ -1,18 +1,3 @@
-insert into owners
-values('sohn', '1234', '손찬영', 'e-mail', '010', 1);
-
-insert into users
-values('user', '1234', '유저', '유저e-mail', '유저폰010', 5);
-
-insert into stores(store_name, store_adress, store_phone, owner_id, region_code, type_code)
-values('가게이름','가게주소','가게번호','sohn',0, 0);
-
-insert into event_comment(title, content, start_date, end_date, store_code, persons_code, service_type_code)
-values('제목','내용',to_date(sysdate, 'yyyy-mm-dd'), to_date(sysdate, 'yyyy-mm-dd'), 1, 0, 0);
-
-insert into review_comment(comment_code, title, content, user_id, star_point, store_code)
-values(1, '제목','내용', 'user', 5, 1);
-
 insert into persons (persons_code, persons_level)
 values(0,'4명이하');
 insert into persons (persons_code, persons_level)
@@ -60,5 +45,20 @@ insert into coupon_status
 values(2,'환불');
 insert into coupon_status
 values(3,'기간 만료');
+
+insert into owners
+values('sohn', '1234', '손찬영', 'e-mail', '010', 1);
+
+insert into users
+values('user', '1234', '유저', '유저e-mail', '유저폰010', 5);
+
+insert into stores(store_name, store_adress, store_phone, owner_id, region_code, type_code)
+values('가게이름','가게주소','가게번호','sohn',0, 0);
+
+insert into event_comment(title, content, start_date, end_date, store_code, persons_code, service_type_code)
+values('제목','내용',to_date(sysdate, 'yyyy-mm-dd'), to_date(sysdate, 'yyyy-mm-dd'), 1, 0, 0);
+
+insert into rview_comment(title, content, user_id, star_point, store_code)
+values('제목','내용', 'user', 5, 1);
 
 
