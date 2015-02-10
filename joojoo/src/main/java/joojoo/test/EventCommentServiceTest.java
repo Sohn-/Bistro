073fe0f0dcaba783dfdcd5415c65ea10d2e418b0
Sohn <ce401-27@ce401-27-HP>
2015-02-10 19:39:10 +0900

    완성

--- joojoo/src/main/java/joojoo/test/EventCommentServiceTest.java
+++ joojoo/src/main/java/joojoo/test/EventCommentServiceTest.java
@@ -1,5 +1,10 @@
 package joojoo.test;
 
+import java.util.List;
+
+import joojoo.entity.Category;
+import joojoo.entity.EventComment;
+import joojoo.entity.Stores;
 import joojoo.exception.ServiceFailException;
 import joojoo.service.EventCommentService;
 
@@ -11,14 +16,14 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class EventCommentServiceTest {
 	
-static final Logger LOG = LoggerFactory.getLogger(DaoTest.class);
+static final Logger logger = LoggerFactory.getLogger(EventCommentServiceTest.class);
 	
 	
 	@Autowired
 	EventCommentService service;
 	
 	public EventCommentServiceTest(){
-		ApplicationContext ctx = new GenericXmlApplicationContext("application_config.xml");
+		ApplicationContext ctx = new GenericXmlApplicationContext("spring/application-config.xml");
 		service = ctx.getBean(EventCommentService.class);
 	}
 	
@@ -32,7 +37,21 @@ static final Logger LOG = LoggerFactory.getLogger(DaoTest.class);
 	
 	
 	public void serviceTest(){		
+/*		List<EventComment> SeachByKeyword(Category category); //키워드로 이벤트 검색
+		List<EventComment> SeachByCategory(Category category); //카테고리 이벤트 검색
+		List<EventComment> SeachMyEvent(String ownerId); //업주회원 내 이벤트 조회
+		int registEvent(EventComment eventComment, Stores store); //업주회원 마이페이지 이벤트 등록
+		int modifyEvent(EventComment eventComment); //업주회원 마이페이지 이벤트 수정
+		int requestDeleteEvent(EventComment eventComment); //업주회원 마이페이지 삭제 요청
+		EventComment SearchOneEvent(int commentCode); //이벤트 하나 상세 조회	
+*/		
+		int commentCode = 1;
+		logger.trace(""+service.SearchOneEvent(commentCode));
 		
+		EventComment eventComment = service.SearchOneEvent(commentCode);
+		logger.trace("수업:"+eventComment);
+		
+		service.requestDeleteEvent(eventComment);
 	
 	}
 
