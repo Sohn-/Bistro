8e09ba313549a35d896af2dab15fcb5b383b95fc
ansoog <ansoog@naver.com>
2015-02-12 15:46:54 +0900

    rview 중간..

--- joojoo/src/main/java/joojoo/controller/RviewCommentController.java
+++ joojoo/src/main/java/joojoo/controller/RviewCommentController.java
@@ -1,13 +1,18 @@
 package joojoo.controller;
 
+import java.util.List;
+
+import joojoo.entity.RviewComment;
 import joojoo.service.RviewCommentService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RviewCommentController {
@@ -18,10 +23,18 @@ public class RviewCommentController {
 	RviewCommentService rviewCommentService;
 	
 	@RequestMapping(value="/review", method=RequestMethod.GET)
-	   public String showSelectJoinType(){
-	      return "review/review";
-	   
-	   }
+	public String showRviewCommentList(Model model){
+		List<RviewComment> rviews = rviewCommentService.SearchAll();
+		model.addAttribute("rviews",rviews);
+		return "review/list";
+	}
+	
+	/*@RequestMapping(value="/review", method=RequestMethod.GET)
+	public String showRviewComment(@RequestParam int commentCode ,Model model){
+		List<RviewComment> rviews = rviewCommentService.SearchAll();
+		model.addAttribute("rviews",rviews);
+		return "review/list";
+	}*/
 	
 	
 }
